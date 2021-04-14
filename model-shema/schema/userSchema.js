7169a92f0b788b47f70f59801415bc1858ca83a0
Svetlana Veklych <veklychsveta@gmail.com>
2021-04-14 13:55:37 +0300

    hw06

--- model-shema/schema/userSchema.js
+++ model-shema/schema/userSchema.js
@@ -28,6 +28,14 @@ const userSchema = new Schema({
   },
   token: String,
   avatarURL: String,
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verifyToken: {
+    type: String,
+    required: [true, 'Verify token is required'],
+  },
 });
 
 userSchema.pre('save', async function (next) {
